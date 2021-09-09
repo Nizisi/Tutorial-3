dd34f923c02dc717b17c906e728a409868c8dbd8
Nizisi <54412715+Nizisi@users.noreply.github.com>
2021-09-08 22:47:05 -0400

    Add files via upload

---
+++ 1st_console_folder/pch.h
@@ -0,0 +1,11 @@
+﻿#ifndef PCH_H
+#define PCH_H
+
+// pch.h
+#pragma once
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Web.Syndication.h>
+#include <iostream>
+
+
+#endif // !PCH_H
